f7a1ba907924a6266c014d774723db5e2628dbb1
Andrew Morawa <andrew80500@gmail.com>
2022-07-18 17:29:50 -0400

    Create bluetoothRCCar.py
    Python bluetooth file

---
+++ bluetoothRCCar.py
@@ -0,0 +1,27 @@
+# https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0
+# For the connection between python and arduino
+import bluetooth
+
+def scan():
+
+    print("Scanning for bluetooth devices:")
+    devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+    number_of_devices = len(devices)
+    names = []
+    info_tuples = []
+    print(number_of_devices,"devices found")
+    for addr, name, device_class in devices:
+        print("\n")
+        print("Device:")
+        print("Device Name: %s" % (name))
+        names.append(name)
+        info_tuples.append((addr, device_class))
+        print("Device MAC Address: %s" % (addr))
+        print("Device Class: %s" % (device_class))
+        print("\n")
+    if 'HC-02' in names:
+        car_info_index = names.index('HC-02')
+        print("Found RC Car")
+        print(info_tuples[car_info_index])
+
+scan()
