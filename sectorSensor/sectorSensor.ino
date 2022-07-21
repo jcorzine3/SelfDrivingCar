66a13dc23af415e81b036f226569b810c1f8f1cb
jcorzine3 <jcorzine3@gatech.edu>
2022-07-21 16:44:21 -0400

    full sensor code added

--- sectorSensor/sectorSensor.ino
+++ sectorSensor/sectorSensor.ino
@@ -2,24 +2,31 @@
 #define trig_PIN1    3  // Ultrasonic Trig pin
 #define echo_PIN2    4
 #define trig_PIN2    5
+#define echo_PIN3    6
+#define trig_PIN3    7
+#define echo_PIN4    8
+#define trig_PIN4    9
 
 long duration;
 int distance;
 int minDistance = 15;
-int sensor1 = 0; // denotes whether sensor 1 was the last sensor triggered
-int sensor2 = 1; // denotes whether sensor 2 was the last sensor triggered
+int prevSensor = 0;
 double lapStart;
 double lapEnd;
 double lapTime;
-double sector1Start;
-double sector1End;
-double sector1Time;
+double sectorStart;
+double sectorEnd;
+double sectorTime;
 
 void setup() {
   pinMode(trig_PIN1, OUTPUT); 
   pinMode(echo_PIN1,INPUT);
   pinMode(trig_PIN2, OUTPUT); 
-  pinMode(echo_PIN2,INPUT);  
+  pinMode(echo_PIN2,INPUT);
+  pinMode(trig_PIN3, OUTPUT); 
+  pinMode(echo_PIN3,INPUT);
+  pinMode(trig_PIN4, OUTPUT); 
+  pinMode(echo_PIN4,INPUT);    
   Serial.begin(9600);
 }
 
@@ -39,36 +46,75 @@ int scan(int trig_PIN, int echo_PIN){
 
 void scanSensor1() {
   int dist = scan(trig_PIN1, echo_PIN1);
-  if (dist < 15 && sensor2 == 1) {
-    lapStart = millis();
-    sector1Start = lapStart;
-    sensor1 = 1;
-    sensor2 = 0;
-    Serial.print("Lap Start: ");
-    Serial.println(lapStart);
+  if (dist < 15) {
+    if (prevSensor == 0) {
+      // start of the race
+      lapStart = millis();
+      sectorStart = lapStart;
+      prevSensor = 1;
+      Serial.print("Lap Start: ");
+      Serial.println(lapStart);
+    } else if (prevSensor == 4) {
+      // next lap
+      lapEnd = millis();
+      sectorEnd = lapEnd;
+      lapTime = lapEnd - lapStart;
+      sectorTime = sectorEnd - sectorStart;
+      sectorStart = sectorEnd;
+      lapStart = lapEnd;
+      prevSensor = 1;
+      Serial.print("Sector 4: ");
+      Serial.println(sectorTime);
+      Serial.print("Lap End: ");
+      Serial.println(lapEnd);
+      Serial.print("Lap Time: ");
+      Serial.println(lapTime);
+      Serial.println();
+      Serial.print("Lap Start: ");
+      Serial.println(lapStart);
+    }
+    
   }
 }
 
 void scanSensor2() {
   int dist = scan(trig_PIN2, echo_PIN2);
-  if (dist < 38 && sensor1 == 1) {
-    lapEnd = millis();
-    sector1End = lapEnd;
-    sensor2 = 1;
-    sensor1 = 0;
-    lapTime = lapEnd - lapStart;
-    sector1Time = lapTime;
-    Serial.print("Lap End: ");
-    Serial.println(lapEnd);
-    Serial.println();
-    Serial.print("Lap Time: ");
-    Serial.println(lapTime);
-    Serial.println();
+  if (dist < 15 && prevSensor == 1) {
+    sectorEnd = millis();
+    sectorTime = sectorEnd - sectorStart;
+    sectorStart = sectorEnd;
+    prevSensor = 2;
+    Serial.print("Sector 1: ");
+    Serial.println(sectorTime);
+  }
+}
+
+void scanSensor3() {
+  int dist = scan(trig_PIN3, echo_PIN3);
+  if (dist < 15 && prevSensor == 2) {
+    sectorEnd = millis();
+    sectorTime = sectorEnd - sectorStart;
+    sectorStart = sectorEnd;
+    prevSensor = 3;
+    Serial.print("Sector 2: ");
+    Serial.println(sectorTime);
+  }
+}
+
+void scanSensor4() {
+  int dist = scan(trig_PIN4, echo_PIN4);
+  if (dist < 15 && prevSensor == 3) {
+    sectorEnd = millis();
+    sectorTime = sectorEnd - sectorStart;
+    sectorStart = sectorEnd;
+    prevSensor = 4;
+    Serial.print("Sector 3: ");
+    Serial.println(sectorTime);
   }
 }
 
 void loop() {
-  //Serial.println(scan(trig_PIN1, echo_PIN1));
+  //Serial.println(scan(trig_PIN3, echo_PIN3));
   //Serial.println(scan(trig_PIN2, echo_PIN2));
   //delay(500);
 
@@ -76,6 +122,8 @@ void loop() {
   delay(10);
   scanSensor2();
   delay(10);
-  
+  scanSensor3();
+  delay(10);
+  scanSensor4();
   
 }
