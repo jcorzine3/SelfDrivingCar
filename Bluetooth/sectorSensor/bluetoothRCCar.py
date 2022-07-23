67e6bbe6f65fa337ee6ff2d92a1778251cfc8f1d
Andrew Morawa <andrew80500@gmail.com>
2022-07-23 17:08:11 -0400

    Update bluetoothRCCar.py
    Allowed for transfer of data from sensor to python

--- Bluetooth/sectorSensor/bluetoothRCCar.py
+++ Bluetooth/sectorSensor/bluetoothRCCar.py
@@ -2,34 +2,38 @@
 # For the connection between python and arduino
 import bluetooth
 import serial
+from pprint import pprint
 import time
 
 #Jesse's port '/devices/cu.usbmodem14201'
 arduino = serial.Serial(port='COM3', baudrate=115200)
+bluetooth_rc_car = serial.Serial('COM7')
 
-def scan():
+# def scan():
 
-    print("Scanning for bluetooth devices:")
-    devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
-    number_of_devices = len(devices)
-    names = []
-    info_tuples = []
-    print(number_of_devices,"devices found")
-    for addr, name, device_class in devices:
-        print("\n")
-        print("Device:")
-        print("Device Name: %s" % (name))
-        names.append(name)
-        info_tuples.append((addr, device_class))
-        print("Device MAC Address: %s" % (addr))
-        print("Device Class: %s" % (device_class))
-        print("\n")
-    if 'HC-02' in names:
-        car_info_index = names.index('HC-02')
-        print("Found RC Car")
-        print(info_tuples[car_info_index])
+#     print("Scanning for bluetooth devices:")
+#     devices = bluetooth.discover_devices(lookup_names = True, lookup_class = True)
+#     number_of_devices = len(devices)
+#     names = []
+#     info_tuples = []
+#     print(number_of_devices,"devices found")
+#     for addr, name, device_class in devices:
+#         print("\n")
+#         print("Device:")
+#         print("Device Name: %s" % (name))
+#         names.append(name)
+#         info_tuples.append((addr, device_class))
+#         print("Device MAC Address: %s" % (addr))
+#         print("Device Class: %s" % (device_class))
+#         print("\n")
+#     if 'HC-02' in names:
+#         car_info_index = names.index('HC-02')
+#         print("Found RC Car")
+#         car_addr, car_class = info_tuples[car_info_index]
+#         service = bluetooth.find_service(address=car_addr)
+#         pprint(service)
 
-#scan()
+# scan()
 
 def write_read():
     data = arduino.readline()
@@ -37,5 +41,9 @@ def write_read():
 
 while True:
     value = write_read()
+    value = value.decode("utf-8")
+    value = value.strip()
+    value = int(float(value))
+    bluetooth_rc_car.write(bin(value))
     print(value)
 
