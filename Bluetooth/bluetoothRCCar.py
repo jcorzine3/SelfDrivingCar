6e56a9e42650ce20fd06b32da0ada9031cb3cc0e
Andrew Morawa <andrew80500@gmail.com>
2022-07-23 18:07:17 -0400

    Added sector location to the bluetooth stream

---
+++ Bluetooth/bluetoothRCCar.py
@@ -0,0 +1,23 @@
+# https://create.arduino.cc/projecthub/ansh2919/serial-communication-between-python-and-arduino-e7cce0
+# For the connection between python and arduino
+import bluetooth
+import serial
+from pprint import pprint
+import time
+
+#Jesse's port '/devices/cu.usbmodem14201'
+arduino = serial.Serial(port='COM3', baudrate=115200)
+bluetooth_rc_car = serial.Serial('COM7')
+
+def write_read():
+    data = arduino.readline()
+    return data
+
+while True:
+    value = write_read()
+    value = value.decode("utf-8")
+    value = value.strip()
+    value = str(int(float(value)))
+    bluetooth_rc_car.write(value.encode("utf-8"))
+    print(value)
+
--- sectorSensor/sectorSensor.ino
+++ sectorSensor/sectorSensor.ino
@@ -11,6 +11,7 @@ long duration;
 int distance;
 int minDistance = 15;
 int prevSensor = 0;
+int sectorLocation = 1;
 double lapStart;
 double lapEnd;
 double lapTime;
@@ -52,8 +53,10 @@ void scanSensor1() {
       lapStart = millis();
       sectorStart = lapStart;
       prevSensor = 1;
-      Serial.print("Lap Start: ");
+      //Serial.print("Lap Start: ");
       Serial.println(lapStart);
+      sectorLocation = 1;
+      Serial.println(sectorLocation); 
     } else if (prevSensor == 4) {
       // next lap
       lapEnd = millis();
@@ -63,15 +66,17 @@ void scanSensor1() {
       sectorStart = sectorEnd;
       lapStart = lapEnd;
       prevSensor = 1;
-      Serial.print("Sector 4: ");
+      //Serial.print("Sector 4: ");
       Serial.println(sectorTime);
-      Serial.print("Lap End: ");
+      //Serial.print("Lap End: ");
       Serial.println(lapEnd);
-      Serial.print("Lap Time: ");
+      //Serial.print("Lap Time: ");
       Serial.println(lapTime);
-      Serial.println();
-      Serial.print("Lap Start: ");
+      //Serial.println();
+      //Serial.print("Lap Start: ");
       Serial.println(lapStart);
+      sectorLocation = 1;
+      Serial.println(sectorLocation); 
     }
     
   }
@@ -84,8 +89,10 @@ void scanSensor2() {
     sectorTime = sectorEnd - sectorStart;
     sectorStart = sectorEnd;
     prevSensor = 2;
-    Serial.print("Sector 1: ");
+    //Serial.print("Sector 1: ");
     Serial.println(sectorTime);
+    sectorLocation = 2;
+    Serial.println(sectorLocation);
   }
 }
 
@@ -96,8 +103,10 @@ void scanSensor3() {
     sectorTime = sectorEnd - sectorStart;
     sectorStart = sectorEnd;
     prevSensor = 3;
-    Serial.print("Sector 2: ");
+    //Serial.print("Sector 2: ");
     Serial.println(sectorTime);
+    sectorLocation = 3;
+    Serial.println(sectorLocation);
   }
 }
 
@@ -108,8 +117,10 @@ void scanSensor4() {
     sectorTime = sectorEnd - sectorStart;
     sectorStart = sectorEnd;
     prevSensor = 4;
-    Serial.print("Sector 3: ");
+    //Serial.print("Sector 3: ");
     Serial.println(sectorTime);
+    sectorLocation = 4;
+    Serial.println(sectorLocation);
   }
 }
 
